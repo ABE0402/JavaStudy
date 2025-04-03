3be2720071278a4901f0e00a19a68243701e7502
ABE0402 <cnrrnquddms@gmail.com>
2025-04-03 18:45:47 +0900

    Add files via upload

---
+++ Workshop8.java
@@ -0,0 +1,17 @@
+class Entry{
+	String word;
+	public void Entry(){
+		
+	}
+	public void writeView() {
+		
+	}
+}
+public class Workshop8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
