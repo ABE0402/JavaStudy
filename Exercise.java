b87f8c07096a07a337f3602d43216fdb3fd9f8de
ABE0402 <cnrrnquddms@gmail.com>
2025-04-04 11:06:51 +0900

    Update Exercise.java

--- Exercise.java
+++ Exercise.java
@@ -1,17 +1,23 @@
 class MyTv2{
-    int channel;
+    int channel; //현재 채널을 저장하는 메소드
 	int prevChannel;	
 	
 	public int getChannel() {
 		return channel;
 	}
 	
+	//현재 채널 : 10
+	//다시 변경할 채널 : 20
+	//현재 채널에서 다른 채널로 채널값을 변경할때 이전채널이 생긴다.
+	
+	//인자로 전달되는 값을 channel 속성값으로 할당하는 메소드
 	public void setChannel(int i) {
 		prevChannel = this.channel;
-		this.channel = i;
+		this.channel = i; //현재 채널 값을 인자로 전달된 값으로 변경
 	}
 	
 	public void gotoPrevChannel() {
+		//setChannel(prevChannel); 이 방법이 더 간단하다. ***
 		int temp = channel; 
 		channel =prevChannel;
 		prevChannel = temp;
